aeedce19ee598f8d8ffcfe9af6ced0782ed0e9cd
SQPeng <1364787288@qq.com>
2018-04-25 14:42:15 +0800

    人员库组件tsc代码

--- es/antd-labCard/index.d.ts
+++ es/antd-labCard/index.d.ts
@@ -1,14 +1,35 @@
 /// <reference types="react" />
 import * as React from 'react';
-export interface Props {
-    cardLink: any;
-    openModal: () => void;
-    handleDelete: () => void;
-    remark: any;
-    arr: string[];
+export interface DataSource {
+    /** card内容描述 */
+    remark?: string;
+    /** 标题 */
+    name?: string;
+    /** 库Id */
+    labId?: number;
+    /** link的地址前缀 */
+    link?: string;
 }
-declare class LabCard extends React.Component<Props> {
-    constructor(props: Props);
+export interface Props {
+    dataSource: DataSource[];
+    /** 监听编辑 */
+    onEdit?: (LabId: number) => void;
+    /** 监听删除 */
+    onDelete?: (LabId: number) => void;
+    /** 头像展示 */
+    arr?: string[];
+    /** 一个卡片里头像的数量 */
+    avatarNumber?: number;
+}
+declare class AntdLabCard extends React.Component<Props> {
+    /**
+     * 删除卡片
+     */
+    handleDelete: (LabId: number) => void;
+    /**
+     * 修改卡片
+     */
+    handleEdit: (LabId: number) => void;
     render(): JSX.Element;
 }
-export default LabCard;
+export default AntdLabCard;
--- es/antd-labCard/index.js
+++ es/antd-labCard/index.js
@@ -10,33 +10,52 @@ var __extends = (this && this.__extends) || (function () {
 })();
 import * as React from 'react';
 import { Card, Icon, Popconfirm, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
 var style = require('./index.scss');
-var LabCard = /** @class */ (function (_super) {
-    __extends(LabCard, _super);
-    function LabCard(props) {
-        return _super.call(this, props) || this;
+var AntdLabCard = /** @class */ (function (_super) {
+    __extends(AntdLabCard, _super);
+    function AntdLabCard() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        /**
+         * 删除卡片
+         */
+        _this.handleDelete = function (LabId) {
+            _this.props.onDelete && _this.props.onDelete(LabId);
+        };
+        /**
+         * 修改卡片
+         */
+        _this.handleEdit = function (LabId) {
+            _this.props.onEdit && _this.props.onEdit(LabId);
+        };
+        return _this;
     }
-    LabCard.prototype.render = function () {
+    AntdLabCard.prototype.render = function () {
         var _this = this;
-        var cardLink = this.props.cardLink;
-        return (React.createElement("div", { className: style['LabCard'] },
-            React.createElement(Card, { title: cardLink, actions: [
-                    React.createElement("div", { key: 'd1', onClick: function () { return _this.props.openModal(); } },
-                        React.createElement(Icon, { type: "edit" }),
-                        "\u00A0\u00A0\u00A0",
-                        React.createElement("span", null, "\u7F16\u8F91")),
-                    React.createElement("div", { key: 'd2' },
-                        React.createElement(Popconfirm, { title: '确认删除?', onConfirm: function () { return _this.props.handleDelete(); } },
-                            React.createElement(Icon, { type: "delete" }),
+        var _a = this.props, dataSource = _a.dataSource, arr = _a.arr;
+        var cardContent = dataSource.map(function (v) {
+            return (React.createElement("div", { key: v.labId },
+                React.createElement(Card, { title: React.createElement(Link, { to: "" + v.link + v.labId }, v.name), actions: [
+                        React.createElement("div", { key: 'd1', onClick: function () { return _this.handleEdit(v.labId); } },
+                            React.createElement(Icon, { type: "edit" }),
                             "\u00A0\u00A0\u00A0",
-                            React.createElement("span", null, "\u5220\u9664")))
-                ] },
-                React.createElement("div", { className: style['LabcardM-B'] + " " + style['personCardItemS'] }, this.props.remark),
-                React.createElement("div", { className: style['personCardItemS'] }, this.props.arr.slice(0, 5).map(function (v, i) {
-                    return (React.createElement(Avatar, { key: i, size: "large", src: "/file/" + v, className: style['Avatar'] }));
-                })))));
+                            React.createElement("span", null, "\u7F16\u8F91")),
+                        React.createElement("div", { key: 'd2' },
+                            React.createElement(Popconfirm, { title: '确认删除?', onConfirm: function () { return _this.handleDelete(v.labId); } },
+                                React.createElement(Icon, { type: "delete" }),
+                                "\u00A0\u00A0\u00A0",
+                                React.createElement("span", null, "\u5220\u9664")))
+                    ] },
+                    React.createElement("div", { className: style['LabcardM-B'] + " " + style['personCardItemS'] }, v.remark),
+                    React.createElement("div", { className: style['personCardItemS'] }, arr
+                        .slice(0, _this.props.avatarNumber || 5)
+                        .map(function (v, i) {
+                        return (React.createElement(Avatar, { key: i, size: "large", src: v, className: style['Avatar'] }));
+                    })))));
+        });
+        return React.createElement("div", { className: style['LabCard'] }, cardContent);
     };
-    return LabCard;
+    return AntdLabCard;
 }(React.Component));
-export default LabCard;
+export default AntdLabCard;
 //# sourceMappingURL=index.js.map
